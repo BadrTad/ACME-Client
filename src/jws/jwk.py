765dc067da1429ebb4755e1a1fefeb8656698108
BadrTad <badrtaddist1@gmail.com>
2023-10-26 16:22:19 +0200

    Add jwk thumprint + Test

--- src/jws/jwk.py
+++ src/jws/jwk.py
@@ -1,23 +1,47 @@
-import copy 
 import json
+import copy
 import url64
+import hashlib
+from typing import Optional
+
 
 class JWKey():
     """JWK implementation."""
 
-    def __init__(self, x: int, y: int) -> None:
-        self.kty: str = "EC"
-        self.crv: str = "P-256"
-        self.use: str = "sig"
-        self.x: bytes = int(x).to_bytes(32, byteorder='big')
-        self.y: bytes = int(y).to_bytes(32, byteorder='big')
+    def __init__(self, **kargs) -> None:
+        self.kty: str = kargs.get('kty', 'EC')
+        self.crv: str = kargs.get('crv', 'P-256' )
+     
+        self.x: str = kargs.get('x')
+        self.y: str = kargs.get('y')
+        
+
+    def thumbprint(self, base64Url = True) -> str|bytes:
+        """Returns the thumbprint of the JWK."""
+
+  
+        jwk_str: str = json.dumps(self.__dict__, sort_keys = True).replace(" ", "").replace("\n", "")
+        jwk_hash = hashlib.sha256(jwk_str.encode("utf-8")).digest()
+        
+        return url64.encode(jwk_hash) if base64Url else jwk_hash
+
+    def __repr__(self) -> str:
+        return f"""
+        JWKey:
+            crv: {self.crv}
+            use: {self.use}
+            kty: {self.kty}
+            x: {self.x}
+            y: {self.y}
+        """
+
+    def as_json(self) -> dict[str,str]:
+        return copy.copy(self.__dict__)
 
 class JWKeyEncoder(json.JSONEncoder):
+    """JSON encoder for JWKey objects."""
     def default(self, obj):
         if isinstance(obj, JWKey):
             d = copy.copy(obj.__dict__)
-            d['x'] = url64.encode(obj.x)
-            d['y'] = url64.encode(obj.y)
-
             return d
-        return json.JSONEncoder.default(self, obj) 
+        return json.JSONEncoder.default(self, obj)
